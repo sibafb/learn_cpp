06e31a3223a72acd56d2453b7809e48dbb04f24c
AkimotoHaruma <siba16fb@gmail.com>
2022-10-26 07:01:03 +0900

    add learning thread.

---
+++ thread/thread.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+using namespace std;
+
+void work(){
+	for(int i = 0; i < 30; i++){
+		cout << std::this_thread::get_id() << " hello world" << endl;
+	}
+}
+
+void work2(){
+	for(int i = 0; i < 30; i++){
+		cout << std::this_thread::get_id() << " good bye world" << endl;
+	}
+}
+
+int main(int argc, char const *argv[]){
+	thread th(work);
+	thread th2(work2);
+
+	th.join();
+	th2.detach();
+    sleep(5);
+	return 0;
+}
